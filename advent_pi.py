40297041a2306cfa66b1a53833723a45dba65d0c
Lasse Gellings <lassegellings03@gmail.com>
2021-11-28 19:36:00 +0000

    addet config.ini and .gitignore

--- advent_pi.py
+++ advent_pi.py
@@ -1,12 +1,18 @@
 """GPIO handling."""
 import time
 import RPi.GPIO as GPIO 
+from configparser import ConfigParser
 
+# read config.ini file
+config_object = ConfigParser()
+config_object.read("config.ini")
+pins = config_object["PINS"]
+time_config = config_object["TIME"]
 
 GPIO.setmode(GPIO.BCM)
 # pin for sensor
-MOVEMENT_PIN = 24
-RELAIS_PIN = 15
+MOVEMENT_PIN = int(pins["movement_pin"])
+RELAIS_PIN = int(pins["relais_pin"])
 
 # disable warnings
 GPIO.setwarnings(False)
@@ -15,9 +21,9 @@ GPIO.setup(MOVEMENT_PIN, GPIO.IN, pull_up_down = GPIO.PUD_UP)
 # configure pin as input pin to be sure relais will not react
 GPIO.setup(RELAIS_PIN, GPIO.IN)
 # time between signal and reaction in seconds
-DELAY_TIME = 1
-LIGHTS_ON_TIME = 15
-start_time = 0
+DELAY_TIME = int(time_config["delay_time"])
+LIGHTS_ON_TIME = int(time_config["lights_on_time"])
+start_time = int(time_config["start_time"])
 
 def on_time_is_over():
     '''True if LIGHTS_ON_TIME is over.'''
@@ -34,4 +40,3 @@ def relais_on():
 def relais_off():
     '''Turns the relais off.'''
     GPIO.setup(RELAIS_PIN, GPIO.IN)
-
